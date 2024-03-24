9e462b8d5d27901d98a677ca0eab83402a411c27
Piotr K <kulicz.pl@gmail.com>
2024-03-24 18:10:09 +0100

    test: refactor

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,28 @@
-describe('Swiper Gallery Test', function () {
-  it('Checks if second slide contains "United Kingdom"', function () {
+describe('Swiper Gallery Test', () => {
+
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
+  })
+  
+  it('Checks if second slide contains "United Kingdom"', () => {
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
-});
 
-describe('Swiper Gallery Test', function () {
-  it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://localhost:3000');
+  it('Checks if third slide contains "Paris"',() => {
     cy.get('.swiper-button-next').click();
-    cy.wait(2000);
+    cy.wait(300);
     cy.get('.swiper-button-next').click({ force: true });
-    cy.wait(2000);
+    cy.wait(300);
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+
+  it('Checks if every slide has correct title', () => {
+    const slideNames = [ "Rome","London", "Paris" ];
+    slideNames.forEach((item) => {
+      cy.get('.swiper-slide-active').should('contain', item);
+      cy.get('.swiper-button-next').click({ force: true });
+      cy.wait(300)
+    });
   });
 });
